091e45008277924c8077a00f1e7087e34cb616d9
Boris <toborto@yandex.ru>
2022-01-25 14:07:54 +0300

    Обновление7

--- src/main/java/ru/curs/mellophone/controller/MellophoneController.java
+++ src/main/java/ru/curs/mellophone/controller/MellophoneController.java
@@ -1,17 +1,27 @@
 package ru.curs.mellophone.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.curs.mellophone.logic.AuthManager;
+
 
 @RestController
 @CrossOrigin
 @RequestMapping("/mellophone")
 public class MellophoneController {
 
-	@GetMapping("/login")
-	public String login() {
 
-		return "Hello login222222222222!";
+    @GetMapping("/login")
+    public String login() {
 
-	}
+        //AuthManager.login(sesid, gp, login, pwd, ip);
+        AuthManager.getTheManager().login("123", "all", "user1", "2222", null);
+
+
+        return "Hello login222222222222!";
+
+    }
 
 }
