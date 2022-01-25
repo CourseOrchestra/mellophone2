95387b0e9d755febb6fab48c35fb9f51d6d869d1
Boris <toborto@yandex.ru>
2022-01-25 15:47:59 +0300

    Update11

--- src/main/java/ru/curs/mellophone/MellophoneApplication.java
+++ src/main/java/ru/curs/mellophone/MellophoneApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MellophoneApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MellophoneApplication.class, args);
     }
-
-
 }
