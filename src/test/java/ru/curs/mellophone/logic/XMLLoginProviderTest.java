e61c217a076e7090b455464dfa51834be6585a50
Boris <toborto@yandex.ru>
2022-02-21 18:48:24 +0300

    XMLLoginProviderTest 5

--- src/test/java/ru/curs/mellophone/logic/XMLLoginProviderTest.java
+++ src/test/java/ru/curs/mellophone/logic/XMLLoginProviderTest.java
@@ -1,5 +1,6 @@
 package ru.curs.mellophone.logic;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Тесты XMLLoginProvider.
  */
+@Disabled
 public class XMLLoginProviderTest extends BaseTestLoginProvider {
 
     private static final String LOGIN = "Петров";
