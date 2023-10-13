34f1aa84693dde4acfea5612f74903c475da669d
mityatube <mityatube@gmail.com>
2023-10-13 14:59:57 +0300

    feat: add gh-pages

--- _builddoc.sh
+++ _builddoc.sh
@@ -1,3 +1,3 @@
-rm -rf ./target
-mkdir target
-docker run --rm -v $PWD:/documents curs/asciidoctor-od asciidoctor doc/pages/manual.adoc -o target/index.html
+rm -rf ./target-doc
+mkdir target-doc
+docker run --rm -v $PWD:/documents curs/asciidoctor-od asciidoctor doc/pages/manual.adoc -o target-doc/index.html
